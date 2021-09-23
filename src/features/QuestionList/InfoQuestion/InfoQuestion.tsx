48c3c798b0a437a982b9eb929f788ecb0e283701
Haii_Ninhh <ninhhd199x@gmail.com>
2021-09-23 09:23:57 +0700

    create library code mirror

--- src/features/QuestionList/InfoQuestion/InfoQuestion.tsx
+++ src/features/QuestionList/InfoQuestion/InfoQuestion.tsx
@@ -1,54 +1,41 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
-
+import parse from "html-react-parser";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
 const InfoQuestion = () => {
-    // useEffect(() => {
-        const getInfoQuestionURL = "http://35.213.94.95:8899/api/questions/513"
-        const cookies = new Cookies();
-        const token = cookies.get("token");
-        const instance = axios.create({
-          baseURL: getInfoQuestionURL,
-          headers: { Authorization: "Bearer " + token },
-        });
-    
-        const question = instance.get("").then((response) => {
-            const {question_text} = response.data;
-            return question_text
-        });
-    //   });
+  const [datas, setDatas] = useState("");
 
-    return (
-        <div className="content">
-            <p>
-                Given two non-negative integers <code>low</code> 
-                and <code>high</code>. Return the <em>count of odd numbers
-                between </em><code>low</code><em> and </em><code>high</code>
-                <em> (inclusive)</em>.
-            </p>
+  useEffect(() => {
+    const getInfoQuestionURL = "http://35.213.94.95:8899/api/questions/513";
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+    const instance = axios.create({
+      baseURL: getInfoQuestionURL,
+      headers: { Authorization: "Bearer " + token },
+    });
+    instance.get("").then((response) => {
+      const { question_text } = response.data;
+      setDatas(question_text);
+    });
+  });
 
-<p> </p>
-<p><strong>Example 1:</strong></p>
-
-<pre><strong>Input:</strong> low = 3, high = 7
-<strong>Output:</strong> 3
-<b>Explanation: </b>The odd numbers between 3 and 7 are [3,5,7].</pre>
-
-<p><strong>Example 2:</strong></p>
-
-<pre><strong>Input:</strong> low = 8, high = 10
-<strong>Output:</strong> 1
-<b>Explanation: </b>The odd numbers between 8 and 10 are [9].</pre>
-
-<p> </p>
-<p><strong>Constraints:</strong></p>
-
-<ul>
-	<li><code>0 &lt;= low &lt;= high &lt;= 10^9</code></li>
-</ul></div>
-    
-    )
-}
-
-export default InfoQuestion
+  return (
+    <div className="content">
+      {parse(datas)}
+      <CodeMirror
+        value="<h1>I ♥ react-codemirror2</h1>"
+        options={{
+          mode: "xml",
+          theme: "material",
+          lineNumbers: true,
+        }}
+        onChange={(editor, data, value) => {}}
+      />
+    </div>
+  );
+};
+export default InfoQuestion;
